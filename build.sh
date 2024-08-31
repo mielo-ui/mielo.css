48b5dee7f9f54938243ffb0775fc216c5e71eff9
Anton Shramko <antonshramko@yandex.ru>
2024-09-01 00:57:59 +0300

    refactor theming

--- build.sh
+++ build.sh
@@ -3,19 +3,13 @@ rimraf='./node_modules/.bin/rimraf'
 sass='./node_modules/.bin/sass'
 
 function build {
-  $sass --load-path=node_modules --quiet-deps --style=expanded --source-map sass/$1.scss css/$1.css
-  $postcss css/$1.css --no-map --use cssnano --output css/$1.min.css
+  $sass --load-path=node_modules --quiet-deps --style=expanded --source-map sass/$1.scss css/$2.css
+  $postcss css/$2.css --no-map --use cssnano --output css/$2.min.css
 }
 
 $rimraf css
 
-build mielo.full &
-
-build mielo &
-build mielo.theme &
-build mielo.variants &
-
-build mielo.material &
-build mielo.material.full;
+build mielo mielo &
+build themes/material/material mielo.material;
 
 wait
--- helpers.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-
-fs.statSync(path.join(__dirname, ""))
